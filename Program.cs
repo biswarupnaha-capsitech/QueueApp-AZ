49cbc35c30ea910b44dfcad7056567bb2105044a
biswarupnaha-capsitech <biswarup.naha@capsitech.com>
2026-09-24 13:04:36 +0530

    insert to queue added

--- Program.cs
+++ Program.cs
@@ -1,9 +1,27 @@
-﻿namespace QueueApp;
+﻿using Azure.Storage.Queues;
+
+namespace QueueApp;
 
 class Program
 {
     public static async Task Main(string[] args)
     {
         Console.WriteLine("QueueApp is running...");
+    }
+
+    static async Task InsertMessageAsync(QueueClient theQueue, string newMessage, bool mustExpire = true, TimeSpan? timeToLive = null)
+    {
+        if (null != await theQueue.CreateIfNotExistsAsync())
+        {
+            Console.WriteLine("The queue was created.");
+        }
+
+        if (mustExpire)
+            if (timeToLive is null)
+                await theQueue.SendMessageAsync(newMessage);
+            else
+                await theQueue.SendMessageAsync(newMessage, timeToLive: timeToLive);
+        else
+            await theQueue.SendMessageAsync(newMessage, default, TimeSpan.FromSeconds(-1), default);
     }
 }
