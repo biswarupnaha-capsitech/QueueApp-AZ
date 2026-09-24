6a905f71741e5f5e2989eb509c52bad106fa2209
biswarupnaha-capsitech <biswarup.naha@capsitech.com>
2026-09-24 13:07:34 +0530

    notes added

--- Program.cs
+++ Program.cs
@@ -9,7 +9,20 @@ class Program
         Console.WriteLine("QueueApp is running...");
     }
 
-    static async Task InsertMessageAsync(QueueClient theQueue, string newMessage, bool mustExpire = true, TimeSpan? timeToLive = null)
+
+
+    /// <summary>
+    /// Inserts a message into the specified Azure Storage Queue. If the queue does not exist, it will be created.
+    /// The message can be set to expire after a specified time-to-live (TTL) duration, or it can be set to never expire.
+    /// A queue message must be in a format compatible with an XML request using UTF-8 encoding.
+    /// A message may be up to 64 KB in size. If a message contains binary data, Base64-encode the message.
+    /// </summary>
+    /// <param name="theQueue">The Azure Storage Queue client.</param>
+    /// <param name="newMessage">The message to insert.</param>
+    /// <param name="mustExpire">By default messages will expire</param>
+    /// <param name="timeToLive">By default TTL is 7 days</param>
+    /// <returns></returns>
+    private static async Task InsertMessageAsync(QueueClient theQueue, string newMessage, bool mustExpire = true, TimeSpan? timeToLive = null)
     {
         if (null != await theQueue.CreateIfNotExistsAsync())
         {
@@ -24,4 +37,6 @@ class Program
         else
             await theQueue.SendMessageAsync(newMessage, default, TimeSpan.FromSeconds(-1), default);
     }
+
+
 }
